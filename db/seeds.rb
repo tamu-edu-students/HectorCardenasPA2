54212e9cda423f8b0023d421e5f3339309de8254
Hector Cardenas <hectorfcardenas@yahoo.com>
2023-09-20 00:04:57 -0500

    updated seeds.rb with more movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,11 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'In the Heights', :rating => 'PG-13',
+    :release_date => '4-Jun-2021'},
+  {:title => 'Jeepers Creepers', :rating => 'R',
+    :release_date => '31-Aug-2001'}
 ]
 
 more_movies.each do |movie|
